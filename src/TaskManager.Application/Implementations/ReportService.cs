bd4b40ebb9001106b9c403db240b67c3513692d8
mitczuck <rafael.mitczuck@gmail.com>
2025-04-22 11:12:55 -0300

    Change project name to a better one

---
+++ src/TaskManager.Application/Implementations/ReportService.cs
@@ -0,0 +1,31 @@
+﻿using TaskManager.Application.Interfaces;
+using TaskManager.Core.DTO.Response;
+using TaskManager.Infrastructure.Interfaces;
+
+namespace TaskManager.Application.Implementations
+{
+    public class ReportService : IReportService
+    {
+        private readonly ITaskRepository _taskRepository;
+
+        public ReportService(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+        public ReportResponse GetCompletedTasksPerUserReport(int UserId)
+        {
+            ReportResponse reportResponse = new ReportResponse();
+
+            try
+            {
+                reportResponse = _taskRepository.GetCompletedTasksPerUserReport(UserId);
+            }
+            catch (Exception e)
+            {
+                reportResponse.Message = e.Message;
+            }
+
+            return reportResponse;
+        }
+    }
+}
